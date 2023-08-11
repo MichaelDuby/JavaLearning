cb8fc1e1be5ddbeadcd248cea0e8d8f64a7eebc8
Syndacyt404 <michaelfduby@outlook.com>
2023-08-11 10:35:48 -0700

    Feat: Add PrimitveDataTypes
    Add PrimitveDataTypes to show how to declare the different types of primitive data types in Java.

---
+++ src/DataTypes/PrimitveDataTypes.java
@@ -0,0 +1,27 @@
+package DataTypes;
+
+public class PrimitveDataTypes {
+    // boolean datatype (true or false)
+    boolean bool = true;
+
+    // byte datatype (whole number between -2^8 to (2^8) -1
+    byte b = 127;
+
+    // char datatype (1 character)
+    char c = 'B';
+
+    // short datatype (whole number between -2^16 to (2^16) -1
+    short d = 32767;
+
+    // int datatype (whole number between -2^32 to (2^32) -1
+    int e = 2147483647;
+
+    // long datatype (whole number between -2^64 to (2^64) -1
+    long g = 9223372036854775807L;
+
+    // float datatype (floating point numbers, less accurate)
+    float f = 1.234F;
+
+    // double datatype (floating point numbers, more accurate)
+    double h = 1.234;
+}
